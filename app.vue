db896721dc0529e00776544b2ff8e138607a2f9a
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-08-07 21:58:18 +0100

    Index page

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <IndexPage />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-
+   <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        Index Page
     </div>
+   </MainLayout>
 </template>
 
 <script setup>
-
+    import MainLayout from '~/layouts/MainLayout.vue';
 </script>
 
 <style lang="scss" scoped>
